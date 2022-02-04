ee63515d993a2af57682c98e776fa99922b95d87
sergey3369 <lernbegierig663@gmail.com>
2022-02-04 21:09:40 +0300

    ,

---
+++ src/main/java/web/service/ServiceCarsImpl.java
@@ -0,0 +1,23 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ServiceCarsImpl implements Service {
+    List<Car> list = Stream.of(
+                    new Car("Car", "Car", "Car"),
+                    new Car("Car1", "Car1", "Car1"),
+                    new Car("Car2", "Car2", "Car2"),
+                    new Car("Car3", "Car3", "Car3"),
+                    new Car("Car4", "Car4", "Car4")
+            )
+            .collect(Collectors.toList());
+
+    @Override
+    public List<? extends Car> getEntity(int quantity) {
+        return list.stream().limit(quantity > 5 ? 5 : quantity).collect(Collectors.toList());
+    }
+}
